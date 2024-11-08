be197f7a8381cdc223b1d01d0a250de01a01eca4
ogiwara <146042372+ogiogidayo@users.noreply.github.com>
2024-11-08 15:56:41 +0900

    fix: clean

--- internal/usecase/usecase.go
+++ internal/usecase/usecase.go
@@ -2,30 +2,35 @@ package usecase
 
 import (
 	"bufio"
+	"context"
 	"github.com/ogiogidayo/docker-scanner/internal/domain"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
 type DockerfileUsecase interface {
-	ParseDockerfile(filePath string) (*domain.DockerfileInfo, error)
+	ParseDockerfile(ctx context.Context, filePath string) (*domain.DockerfileInfo, error)
+	FindDockerfiles(ctx context.Context) ([]string, error)
 }
 
 type dockerfileUsecase struct{}
 
-func NewDockerfileUsecase() DockerfileUsecase {
+func NewDockerfileUsecase() *dockerfileUsecase {
 	return &dockerfileUsecase{}
 }
 
-func (u *dockerfileUsecase) ParseDockerfile(filePath string) (*domain.DockerfileInfo, error) {
+func (u *dockerfileUsecase) ParseDockerfile(_ context.Context, filePath string) (*domain.DockerfileInfo, error) {
 	file, err := os.Open(filePath)
 	if err != nil {
 		return nil, err
 	}
-	defer file.Close()
+	defer func(file *os.File) {
+		_ = file.Close()
+	}(file)
 
 	scanner := bufio.NewScanner(file)
-	var from, cmd string
+	var from, cmd, run string
 
 	for scanner.Scan() {
 		line := strings.TrimSpace(scanner.Text())
@@ -33,6 +38,8 @@ func (u *dockerfileUsecase) ParseDockerfile(filePath string) (*domain.Dockerfile
 			from = strings.TrimPrefix(line, "FROM ")
 		} else if strings.HasPrefix(line, "CMD ") {
 			cmd = strings.TrimPrefix(line, "CMD ")
+		} else if strings.HasPrefix(line, "RUN") {
+			run = strings.TrimPrefix(line, "RUN")
 		}
 	}
 
@@ -40,5 +47,28 @@ func (u *dockerfileUsecase) ParseDockerfile(filePath string) (*domain.Dockerfile
 		return nil, err
 	}
 
-	return &domain.DockerfileInfo{From: from, Cmd: cmd}, nil
+	a := domain.DockerfileInfo{From: from, Cmd: cmd, Run: run}
+
+	npms := domain.NewNpmPackages()
+	npms.ExtractNpmPackages(a)
+	npms.PrintNpmPackages()
+
+	return &domain.DockerfileInfo{From: from, Cmd: cmd, Run: run}, nil
+}
+
+func (u *dockerfileUsecase) FindDockerfiles(_ context.Context) ([]string, error) {
+	var dockerfiles []string
+	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.Name() == "Dockerfile" {
+			dockerfiles = append(dockerfiles, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return dockerfiles, nil
 }
